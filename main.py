3a90b0b69e29b82f92eb5ec6f9d79b00d1e1072f
steveleecode <125843025+steveleecode@users.noreply.github.com>
2025-03-25 13:49:55 -0700

    Added Login Logic :

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 import sqlite3
+import random
+import string
+from redis import Redis
+import hashlib
 
 app = Flask(__name__)
+
+SESSION_TYPE = 'redis'
+SESSION_REDIS = Redis(host='localhost', port=6379)
+app.config.from_object(__name__)
+Session(app)
+
+length = 8
+random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 # Function to initialize the database and create the tasks table if it doesn't exist
 def init_db():
     conn = sqlite3.connect("tasks.db")  # Connect to the SQLite database
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS tasks (
-                        id INTEGER PRIMARY KEY, 
+                        id INTEGER PRIMARY KEY AUTOINCREMENT, 
                         task TEXT,
                         priority TEXT)''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT, 
+                    password TEXT,
+                    email TEXT)''')
     conn.commit()  # Save changes
     conn.close()  # Close database connection
 
@@ -49,7 +67,66 @@ def complete_task():
 @app.route("/", methods=["GET"])
 def index():
     tasks = get_tasks()  # Fetch all tasks from the database
-    return render_template("index.html", tasks=tasks)  # Render the HTML page with tasks
+    return render_template("index.html", tasks=tasks, random_string = random_string)  # Render the HTML page with tasks
+
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/login_user", methods=["POST"])
+def login_user():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    email = request.form.get("email")
+
+    password_hashed = password
+    #hashlib.sha256(password.encode()).hexdigest()
+
+    conn = sqlite3.connect("tasks.db")  # Connect to the database
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users where username = ? and password = ?", (username, password_hashed))
+    output = cursor.fetchone()
+    
+    if output == None:
+        print("User password combination not found")
+        return redirect("/login")
+
+    user = output[1]
+    password = output[2]
+    conn.commit()  # Save changes
+    conn.close()  # Close connection
+
+
+
+    if user == None or password == None:
+        print("User not found")
+        return redirect("/login")
+    elif password == password_hashed:
+        #session["logged_in"] = True
+        return redirect("/")
+
+@app.route("/register", methods=["GET"])
+def register():
+    return render_template("register.html")
+
+@app.route("/register_user", methods=["POST"])
+def register_user():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    password_hashed = hashlib.sha256(password.encode()).hexdigest()
+
+    conn = sqlite3.connect("tasks.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users where username = ? and password = ?", (username, password_hashed))
+
+    if cursor.fetchone() is not None:
+        return redirect("/register")
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password_hashed))
+    conn.commit()
+    conn.close()
+    return redirect("/login")
 
 # Route to handle task submissions
 @app.route("/add_task", methods=["POST"])
